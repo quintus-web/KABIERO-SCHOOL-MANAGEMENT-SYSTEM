ddc1ea26bbe5c3adf98d11b67b5df79a031fe856
quintus-web <ongangaquintus@gmail.com>
2026-06-08 10:37:40 +0300

    Update settings.py

--- sms_core/settings.py
+++ sms_core/settings.py
@@ -117,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# 🎯 FIXED: Tells Render exactly where to compile static asset stylesheets
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
